b6cdf6c6a6d8281b2badb98dd1babefc9841c3cb
Daniel Cruz <bladoxp@gmail.com>
2018-05-23 00:25:09 +0200

    Summary x GOcat of performance

--- src/be/ugent/psb/other/GOPredictPerformanceExtractor.java
+++ src/be/ugent/psb/other/GOPredictPerformanceExtractor.java
@@ -2,9 +2,11 @@ package be.ugent.psb.other;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,10 +26,13 @@ public class GOPredictPerformanceExtractor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String inFolder = args[0];
-		String fileOut = args[2];
-		String godesc = args[1];
+		//		String fileOut = args[1];//.replace(" ", "");
+		String godescFile = args[1];
+		//change here for only one GO cat
+		String godesc = null;
 		String filename;
 		String onlyname;
+
 
 		File file = null;
 		File[] files = null;
@@ -38,48 +43,86 @@ public class GOPredictPerformanceExtractor {
 		double precision [] = null;
 		double fmeasure [] = null;
 
-		try(PrintWriter outFileREC = new PrintWriter(new FileOutputStream(inFolder+file.separatorChar+fileOut+"REC.tsv"));
-				PrintWriter outFilePREC = new PrintWriter(new FileOutputStream(inFolder+file.separatorChar+fileOut+"PREC.tsv"));
-				PrintWriter outFileFMES = new PrintWriter(new FileOutputStream(inFolder+file.separatorChar+fileOut+"FMES.tsv"))){
-			if (inFolder != null && !inFolder.equals("")) {
-				file = new File(inFolder);
-				//look for specific extension
-				if (file.exists()) {
-					files = file.listFiles(new FilenameFilter() {
-						public boolean accept(File dir, String name) {
-							return name.toString().toLowerCase().endsWith("_pingo.txt");
+		try(BufferedReader inDescFile = new BufferedReader(new FileReader(godescFile))){
+			while ((godesc = inDescFile.readLine()) != null) {
+				
+				try(PrintWriter outFileREC = new PrintWriter(new FileOutputStream(inFolder+file.separatorChar+godesc+"REC.tsv"));
+						PrintWriter outFilePREC = new PrintWriter(new FileOutputStream(inFolder+file.separatorChar+godesc+"PREC.tsv"));
+						PrintWriter outFileFMES = new PrintWriter(new FileOutputStream(inFolder+file.separatorChar+godesc+"FMES.tsv"))){
+					if (inFolder != null && !inFolder.equals("")) {
+						file = new File(inFolder);
+						//look for specific extension
+						if (file.exists()) {
+							files = file.listFiles(new FilenameFilter() {
+								public boolean accept(File dir, String name) {
+									return name.toString().toLowerCase().endsWith("_pingo.txt");
+								}
+							});
+
 						}
-					});
+					}
 
-				}
-			}
+					List<File> listFiles = new ArrayList<File>(Arrays.asList(files));
 
-			List<File> listFiles = new ArrayList<File>(Arrays.asList(files));
+					for (int i=0;i<listFiles.size();i++) {
+						recall = new double[10];
+						precision = new double[10];
+						fmeasure = new double[10];
+						filename = listFiles.get(i).getAbsolutePath();
+						onlyname = listFiles.get(i).getName().split("_")[0];
 
-			for (int i=0;i<listFiles.size();i++) {
-				recall = new double[10];
-				precision = new double[10];
-				fmeasure = new double[10];
-				filename = listFiles.get(i).getAbsolutePath();
-				onlyname = listFiles.get(i).getName().split("_")[0];
+						try(BufferedReader inFile = new BufferedReader(new FileReader(filename))){
+							while ((str = inFile.readLine()) != null) {
+								//						if(str.contains("C4 photosynthesis")){
+								if(str.contains(godesc)){
+									splitLine = str.split("\t");
 
-				try(BufferedReader inFile = new BufferedReader(new FileReader(filename))){
-					while ((str = inFile.readLine()) != null) {
-//						if(str.contains("C4 photosynthesis")){
-						if(str.contains(godesc)){
-							splitLine = str.split("\t");
+									//add values into arrays to operate them
+									for(int o=0;o<10;o++){
+										recall[o]=Double.parseDouble(splitLine[o+6]);
+										precision[o]=Double.parseDouble(splitLine[o+16]);
+										fmeasure[o]=2*((precision[o]*recall[o])/(precision[o]+recall[o]));
 
-						}	
+									}
+
+									//print RECALL 
+									outFileREC.print(onlyname);
+									for (double num : recall) {
+										outFileREC.print("\t"+num);	
+									}
+									outFileREC.println();
+
+									//print PRECISION 
+									outFilePREC.print(onlyname);
+									for (double num : precision) {
+										outFilePREC.print("\t"+num);	
+									}
+									outFilePREC.println();
+
+									//print FMEASURE 
+									outFileFMES.print(onlyname);
+									for (double num : fmeasure) {
+										outFileFMES.print("\t"+num);	
+									}
+									outFileFMES.println();
+
+
+								}	
+
+							}
+						}
 
 					}
+				}catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				
-			}
-		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+			}
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 	}
 
