95b67fbce72a83eed97511441bd642631e1c0f5a
dacru <dacru@psb.vib-ugent.be>
2019-08-13 11:22:19 +0200

    Change for Heike search, and finding annotation for some genes

--- src/be/ugent/psb/go/GOinfoFinder.java
+++ src/be/ugent/psb/go/GOinfoFinder.java
@@ -6,26 +6,55 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Set;
 
 public class GOinfoFinder {
-
+/*
+ * Class to get data from GO terms, options in arg 2 name or code
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//For Heike favor I moved args 0 - 1 
 		try(BufferedReader inFile = new BufferedReader(new FileReader(args[0]));
 				PrintWriter outFile = new PrintWriter(new FileOutputStream(args[0]+".ID"))){
-			HashMap<String, GoTerm> ontology = GoDescLoader.loadOntologyNameKey(args[1]);
+			
+			
+			String choice = args[2];
 			
 			String str;
-			String go_name;
+			String go_key;
 			GoTerm tmpTerm;
+			HashMap<String, GoTerm> ontologyName = null;
+			HashMap<Integer, GoTerm> ontologyId = null;
 			
-			while ((str = inFile.readLine()) != null) {
-				go_name = str.split("\t")[0];
-				tmpTerm = ontology.get(go_name);
-				outFile.println(tmpTerm.getId()+"\t"+str);
+			if(choice.equals("name")) {
+				ontologyName = GoDescLoader.loadOntologyNameKey(args[1]);
+				while ((str = inFile.readLine()) != null) {
+					go_key = str.split("\t")[0];
+					tmpTerm = ontologyName.get(go_key);
+					outFile.println(tmpTerm.getId()+"\t"+str);
+				}	
+			}
+			if(choice.equals("id")) {
+				ontologyId = GoDescLoader.loadOntology(args[1]);
+				while ((str = inFile.readLine()) != null) {
+					go_key = str.split("\t")[0];
+					tmpTerm = ontologyId.get(Integer.parseInt(go_key));
+					outFile.println(tmpTerm.getName()+"\t"+str);
+				}
 			}
 			
-		
+			if(choice.equals("allOnto")) {
+				ontologyId = GoDescLoader.loadOntology(args[1]);
+				Set<Integer> ids = ontologyId.keySet();
+				
+				for(int id : ids) {
+					tmpTerm = ontologyId.get(id);
+					outFile.println(tmpTerm.getId()+"\t"+tmpTerm.getName()+"\t"+tmpTerm.getNameSpace());
+				}
+				
+				
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/be/ugent/psb/go/GoDescLoader.java
+++ src/be/ugent/psb/go/GoDescLoader.java
@@ -32,8 +32,8 @@ public class GoDescLoader {
 		try {
 
 
-//			loadShownAnnotPlusCommonNamePlusDesc(args[0], args[1], args[2],args[3],args[4]);
-			loadShownAnnotPlusCommonNamePlusDescWithTargetGOs(args[0], args[1], args[2],args[3],args[4],args[5]);
+			loadShownAnnotPlusCommonNamePlusDesc(args[0], args[1], args[2],args[3],args[4]);
+//			loadShownAnnotPlusCommonNamePlusDescWithTargetGOs(args[0], args[1], args[2],args[3],args[4],args[5]);
 
 
 			
