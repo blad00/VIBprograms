3c706f75b264d4b8facd0434acd5076b51f17f50
Daniel Cruz <bladoxp@gmail.com>
2020-06-19 17:54:54 +0200

    general test

---
+++ src/be/ugent/psb/tester/Testo.java
@@ -0,0 +1,11 @@
+package be.ugent.psb.tester;
+
+public class Testo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = Integer.parseInt("NaN");
+		System.out.println(0+num);
+	}
+
+}
