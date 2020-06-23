fa31929a5f2fc3a297f87a5b263eda15007d1d5b
dacru <dacru@psb.vib-ugent.be>
2020-06-23 18:17:22 +0200

    Class to get top N best peacor edges

---
+++ src/be/ugent/psb/network/PearsonCoresult.java
@@ -0,0 +1,31 @@
+package be.ugent.psb.network;
+
+public class PearsonCoresult {
+
+	private String gene1;
+	private String gene2;
+	private double corr;
+	
+	
+	public String getGene1() {
+		return gene1;
+	}
+	public void setGene1(String gene1) {
+		this.gene1 = gene1;
+	}
+	public String getGene2() {
+		return gene2;
+	}
+	public void setGene2(String gene2) {
+		this.gene2 = gene2;
+	}
+	public double getCorr() {
+		return corr;
+	}
+	public void setCorr(double corr) {
+		this.corr = corr;
+	}
+
+
+	
+}
